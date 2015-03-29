90115800b8bb686674d992b7a14cf50216f61ec3
Tomona Nanase <nanase@users.noreply.github.com>
2015-03-29 22:48:01 +0900

    単体テストプロジェクトを追加

---
+++ UnitTest/LexerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Lury.Compiling.Lexer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void TokenizeTest1()
+        {
+            var lexer = new Lexer("");
+            lexer.Tokenize();
+
+            Assert.IsTrue(lexer.TokenOutput.Select(t => t.Entry.Name).SequenceEqual(new string[] { "EndOfFile" }));
+        }
+    }
+}
