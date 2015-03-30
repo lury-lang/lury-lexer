b70dda803bc9517f8669cb74490daec8cd5a10b9
Tomona Nanase <nanase@users.noreply.github.com>
2015-03-30 22:51:02 +0900

    AnswerクラスおよびAnswerFileクラスを追加

---
+++ UnitTest/Answer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class Answer
+    {
+        public string TokenName { get; private set; }
+
+        public string TokenValue { get; private set; }
+
+        public Answer(string tokenName, string tokenValue)
+        {
+            this.TokenName = tokenName;
+            this.TokenValue = tokenValue;
+        }
+
+        public static IReadOnlyList<Answer> FromFile(string filePath)
+        {
+            List<Answer> answers = new List<Answer>();
+
+            using (FileStream stream = new FileStream(filePath, FileMode.Open))
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                string line;
+                int sep;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if ((sep = line.IndexOf('\t')) == -1)
+                    {
+                        answers.Add(new Answer(line, null));
+                    }
+                    else
+                    {
+                        answers.Add(new Answer(line.Substring(0, sep), ReplaceEscapeChars(line.Substring(sep + 1))));
+                    }
+                }
+            }
+
+            return answers;
+        }
+
+        private static string ReplaceEscapeChars(string input)
+        {
+            return input.Replace("\\r", "\r").Replace("\\n", "\n").Replace("\\t", "\t");
+        }
+    }
+}
---
+++ UnitTest/AnswerFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    class AnswerFile
+    {
+        public IReadOnlyList<Answer> Answers { get; private set; }
+
+        public string AnswerFilePath { get; private set; }
+
+        public string TestFilePath
+        {
+            get
+            {
+                return Path.Combine(Path.GetDirectoryName(this.AnswerFilePath), Path.GetFileNameWithoutExtension(this.AnswerFilePath) + ".lr");
+            }
+        }
+
+        public AnswerFile(string answerFilePath)
+        {
+            this.AnswerFilePath = answerFilePath;
+            this.Answers = Answer.FromFile(answerFilePath);
+        }
+    }
+}
