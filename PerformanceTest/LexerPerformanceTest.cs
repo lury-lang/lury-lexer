7e833dea454af10693419b706d6a6504a561fdcb
Tomona Nanase <nanase@users.noreply.github.com>
2015-04-13 16:05:08 +0900

    Add a project 'PerformanceTest'

---
+++ PerformanceTest/LexerPerformanceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Lury.Compiling.Lexer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PerformanceTest
+{
+    [TestClass]
+    public class LexerPerformanceTest
+    {
+        private static string inputSouceCode;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            LexerPerformanceTest.inputSouceCode = System.IO.File.ReadAllText("Input.lr");
+        }
+
+        [TestMethod]
+        public void TokenizeTest()
+        {
+            const int count = 100;
+
+            for (int i = 0; i < count; i++)
+            {
+                Lexer lexer = new Lexer(inputSouceCode);
+                lexer.Tokenize();
+            }
+        }
+    }
+}
---
+++ PerformanceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("PerformanceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("PerformanceTest")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照できなくなります。このアセンブリ内で COM から型にアクセスする必要がある場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+[assembly: Guid("e4596180-aa5f-4248-a787-deb410c66be1")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、以下のように '*' を使用してビルド番号とリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
