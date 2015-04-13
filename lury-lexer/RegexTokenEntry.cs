abe50ebdcc8e500ca5722d1ae4405922ca1f1380
Tomona Nanase <nanase@users.noreply.github.com>
2015-04-14 01:57:03 +0900

    Add a file 'RegexTokenEntry.cs'

---
+++ lury-lexer/RegexTokenEntry.cs
@@ -0,0 +1,62 @@
+﻿//
+// TokenEnrty.cs
+//
+// Author:
+//       Tomona Nanase <nanase@users.noreply.github.com>
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Tomona Nanase
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System.Text.RegularExpressions;
+
+namespace Lury.Compiling.Lexer
+{
+    public class RegexTokenEntry : TokenEntry
+    {
+        #region -- Public Properties --
+
+        public Regex Regex { get; private set; }
+
+        #endregion
+
+        #region -- Constructors --
+
+        public RegexTokenEntry(string name)
+            : base(name)
+        {
+            this.Regex = null;
+        }
+
+        public RegexTokenEntry(string name, Regex regex)
+            : base(name)
+        {
+            this.Regex = regex;
+        }
+
+        public RegexTokenEntry(string name, string regex)
+            : this(name, new Regex(regex, RegexOptions.Compiled | RegexOptions.ExplicitCapture))
+        {
+        }
+
+        #endregion
+    }
+}
