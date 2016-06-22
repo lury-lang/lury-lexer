b29597c2c6d6180a251ccaa6b5993372424f2778
Tomona Nanase <nanase@users.noreply.github.com>
2016-06-23 01:29:24 +0900

    modify: TokenTestのR#er対応

--- UnitTest/TokenTest.cs
+++ UnitTest/TokenTest.cs
@@ -8,35 +8,36 @@ namespace UnitTest
     [TestFixture]
     public class TokenTest
     {
-        const string Name = "dummyName";
-        const string NameShort = "n";
-        const string Regex = "dummyRegex";
-        const string Source = "dummyText";
-        const int Index = 2;
-        const int Length = 3;
-        static CharPosition position;
-        static RegexTokenEntry entry, entryShort;
+        private const string Name = "dummyName";
+        private const string NameShort = "n";
+        private const string Regex = "dummyRegex";
+        private const string Source = "dummyText";
+        private const int Index = 2;
+        private const int Length = 3;
+        private static CharPosition _position;
+        private static RegexTokenEntry _entry;
+        private static RegexTokenEntry _entryShort;
 
         [OneTimeSetUp]
         public static void ClassInitialize(TestContext context)
         {
-            position = new CharPosition(1, 3);
-            entry = new RegexTokenEntry(Name, Regex);
-            entryShort = new RegexTokenEntry(NameShort, Regex);
+            _position = new CharPosition(1, 3);
+            _entry = new RegexTokenEntry(Name, Regex);
+            _entryShort = new RegexTokenEntry(NameShort, Regex);
         }
 
         [Test]
         public void LengthTest()
         {
-            Token token = new Token(entry, string.Empty, Source, Index, Length);
+            var token = new Token(_entry, string.Empty, Source, Index, Length);
             Assert.AreEqual(Length, token.Length);
         }
 
         [Test]
         public void EntryTest()
         {
-            Token token = new Token(entry, string.Empty, Source, Index, Length);
-            Assert.AreEqual(entry, token.Entry);
+            var token = new Token(_entry, string.Empty, Source, Index, Length);
+            Assert.AreEqual(_entry, token.Entry);
         }
 
         [Test]
@@ -44,14 +45,14 @@ namespace UnitTest
         [TestCase("")]
         public void TextTest(string tokenText)
         {
-            Token token = new Token(entry, tokenText, Source, Index, Length);
+            var token = new Token(_entry, tokenText, Source, Index, Length);
             Assert.AreEqual(tokenText, token.Text);
         }
 
         [Test]
         public void IndexTest()
         {
-            Token token = new Token(entry, string.Empty, Source, Index, Length);
+            var token = new Token(_entry, string.Empty, Source, Index, Length);
             Assert.AreEqual(Index, token.Index);
         }
 
@@ -61,48 +62,49 @@ namespace UnitTest
         [TestCase("", 0, 0, 1, 1)]
         public void PositionTest(string sourceCode, int index, int length, int line, int column)
         {
-            Token token = new Token(entry, string.Empty, sourceCode, index, length);
+            var token = new Token(_entry, string.Empty, sourceCode, index, length);
             Assert.AreEqual(new CharPosition(line, column), token.CodePosition.CharPosition);
         }
 
         [Test]
         public void ToStringTest()
         {
-            Token token = new Token(entry, string.Empty, Source, Index, Length);
-            string tokenString = token.ToString();
+            var token = new Token(_entry, string.Empty, Source, Index, Length);
+            var tokenString = token.ToString();
 
-            Assert.IsTrue(tokenString.Contains(position.Line.ToString()));
-            Assert.IsTrue(tokenString.Contains(position.Column.ToString()));
-            Assert.IsTrue(tokenString.Contains(entry.Name));
+            Assert.IsTrue(tokenString.Contains(_position.Line.ToString()));
+            Assert.IsTrue(tokenString.Contains(_position.Column.ToString()));
+            Assert.IsTrue(tokenString.Contains(_entry.Name));
             Assert.IsTrue(tokenString.Contains("mmy"));
         }
 
         [Test]
         public void ToStringTestShort()
         {
-            Token token = new Token(entryShort, string.Empty, Source, Index, Length);
-            string tokenString = token.ToString();
+            var token = new Token(_entryShort, string.Empty, Source, Index, Length);
+            var tokenString = token.ToString();
 
-            Assert.IsTrue(tokenString.Contains(position.Line.ToString()));
-            Assert.IsTrue(tokenString.Contains(position.Column.ToString()));
-            Assert.IsTrue(tokenString.Contains(entryShort.Name));
+            Assert.IsTrue(tokenString.Contains(_position.Line.ToString()));
+            Assert.IsTrue(tokenString.Contains(_position.Column.ToString()));
+            Assert.IsTrue(tokenString.Contains(_entryShort.Name));
         }
 
         private static readonly TestCaseData[] ConstructorErrorTestCases =
         {
-            new TestCaseData(entry, string.Empty, Source, Source.Length, 1),
+            new TestCaseData(_entry, string.Empty, Source, Source.Length, 1),
             new TestCaseData(null, string.Empty, Source, 0, 1),
-            new TestCaseData(entry, null, Source, 0, 1),
-            new TestCaseData(entry, string.Empty, null, 0, 1),
-            new TestCaseData(entry, string.Empty, Source, -1, 1),
-            new TestCaseData(entry, string.Empty, Source, Source.Length + 1, -1),
-            new TestCaseData(entry, string.Empty, Source, 0, -1)
+            new TestCaseData(_entry, null, Source, 0, 1),
+            new TestCaseData(_entry, string.Empty, null, 0, 1),
+            new TestCaseData(_entry, string.Empty, Source, -1, 1),
+            new TestCaseData(_entry, string.Empty, Source, Source.Length + 1, -1),
+            new TestCaseData(_entry, string.Empty, Source, 0, -1)
         };
 
         [Test]
         [TestCaseSource(nameof(ConstructorErrorTestCases))]
         public void ConstructorError(TokenEntry entry, string sourceName, string sourceCode, int index, int length)
         {
+            // ReSharper disable once ObjectCreationAsStatement
             Assert.Throws<ArgumentOutOfRangeException>(() => new Token(entry, sourceName, sourceCode, index, length));
         }
     }
